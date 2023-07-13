dc24e4b2aabfe0e635aa92318d674939c578912c
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-13 23:42:40 +0300

    add skills responsive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,11 +3,33 @@ import { Container, Row } from 'react-bootstrap';
 import 'react-multi-carousel/lib/styles.css';
 
 export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: {max: 4000, min: 3000},
+            items: 5,
+        },
+        desktop: {
+            breakpoint: {max: 3000, min: 1024},
+            items: 3,
+        },
+        tablet: {
+            breakpoint: {max: 1024, min: 464},
+            items: 2,
+        },
+        mobile: {
+            breakpoint: {max: 464, min: 0},
+            items: 1,
+        },
+    }
+
+
+
+
   return (
     <section className='skill' id="skells">
     <Container>
         <Row>
-            
+
         </Row>
     </Container>
 
