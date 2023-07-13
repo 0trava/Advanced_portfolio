97dce2c05ab13857378c3bcc7de73e67978620de
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-13 23:38:56 +0300

    add skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import 'react-multi-carousel/lib/styles.css';
 
 export const Skills = () => {
   return (
-    <div>Skills</div>
+    <section className='skill' id="skells">
+    <Container>
+        <Row>
+            
+        </Row>
+    </Container>
+
+    </section>
   )
 }
