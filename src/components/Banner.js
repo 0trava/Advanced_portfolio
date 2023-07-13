df74edfec7843251503cf3620cbfb32da58da6d4
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-13 22:59:24 +0300

    add

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+// import {ArrowRightCircle} from "react-bootstrap-icons";
+
 
 export const Banner = () => {
   return (
     <section className='banner' id="home">
+        <Container>
         <Row className='align-items-center'>
             <Col xs={12} md={6} xl={7}>
+                <span className='tagline'>Welcome to my Portfolio</span>
+                <h1>{`Hi I'm webdecoded - 0trava`} </h1>
+                <p>It doesn't matter how many days in your life.It is important how much life is in your days</p>
+                <button onClick={() => {}}>Let's connect</button>
+            </Col>
+            <Col>
             </Col>
 
         </Row>
+        </Container>
     </section>
   )
 }
