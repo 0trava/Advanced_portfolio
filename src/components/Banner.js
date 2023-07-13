33a7d1757e5cc6950c2b4dc96e8b1e00e45da8ea
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-13 23:16:49 +0300

    add

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -24,7 +24,13 @@ export const Banner = () => {
     const tick = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
-        
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText (updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
     }
 
   return (
