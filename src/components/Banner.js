053594c6ebe2f077704eeef7396b2e2fff3c6a0e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-13 23:31:23 +0300

    add

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,66 +1,81 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import {ArrowRightCircle} from "react-bootstrap-icons";
-import HeaderImg from "../assets/img/header-img.svg";
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+// import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
 
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
     const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300-Math.random() * 100);
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
     const period = 2000;
-
+  
     useEffect(() => {
-        let ticker = setInterval(() => {
-            ticker();
-        }, delta )
-
-        return () => {clearInterval(ticker)};
+      let ticker = setInterval(() => {
+        tick();
+      }, delta);
+  
+      return () => { clearInterval(ticker) };
     }, [text])
-
+  
     const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-        setText (updatedText);
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2)
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setDelta(period);
-
-        }else if (isDeleting && updatedText === "") {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setDelta(500);
-        }
+      let i = loopNum % toRotate.length;
+      let fullText = toRotate[i];
+      let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+  
+      setText(updatedText);
+  
+      if (isDeleting) {
+        setDelta(prevDelta => prevDelta / 2);
+      }
+  
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setIndex(prevIndex => prevIndex - 1);
+        setDelta(period);
+      } else if (isDeleting && updatedText === '') {
+        setIsDeleting(false);
+        setLoopNum(loopNum + 1);
+        setIndex(1);
+        setDelta(500);
+      } else {
+        setIndex(prevIndex => prevIndex + 1);
+      }
     }
-
-  return (
-    <section className='banner' id="home">
+  
+    return (
+      <section className="banner" id="home">
         <Container>
-        <Row className='align-items-center'>
+          <Row className="aligh-items-center">
             <Col xs={12} md={6} xl={7}>
-                <span className='tagline'>Welcome to my Portfolio</span>
-                <h1>{`Hi I'm webdecoded - 0trava`}<span className='wrap'>{text}</span></h1>
-                <p>It doesn't matter how many days in your life.It is important how much life is in your days</p>
-                <button onClick={() => {}}>Let's connect 
-                <ArrowRightCircle size={25}/>
-                </button>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>{`Hi! I'm 0trava`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                    <p>I believe in logic and acquired skills.
+                        Most of all, I like to learn and understand something new, to solve the tasks.
+                        I'm currently learning everything I can get my hands on.</p>
+                    <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                </div>}
+              </TrackVisibility>
             </Col>
             <Col xs={12} md={6} xl={5}>
-                <img src={HeaderImg} alt="Header Img"/>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                    <img src={headerImg} alt="Header Img"/>
+                  </div>}
+              </TrackVisibility>
             </Col>
-
-        </Row>
+          </Row>
         </Container>
-    </section>
-  )
-}
+      </section>
+    )
+  }
