7712cdd3c74825dae962b5ef684a0f5bba22f948
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-14 22:34:11 +0300

    add projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, Nav, Tab } from 'react-bootstrap';
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
@@ -43,11 +43,45 @@ export const Projects = () => {
       ];
 
   return (
-    <div>
+    <section className='project' id="project">
         <Container>
+            <Row>
+                <Col>
+                <h2>Projects</h2>
+                <p>Lorem Ipsum</p>
+
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="first">
+                        <Row>
+                            {projects.map((project, index) => {
+                                return (
+                                    <p>{project.title}</p>
+                                )
+                            })}
+                        </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">Lorem Ipsum</Tab.Pane>
+                    <Tab.Pane eventKey="third">Lorem Ipsum</Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+                
+                </Col>
+            </Row>
 
         </Container>
 
-    </div>
+    </section>
   )
 }
