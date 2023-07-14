e7f1e83ddc1b67ed32103403e088c68862dc113b
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-14 22:27:11 +0300

    add projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { NavBar } from "./components/NavBar";
 import {Banner} from "./components/Banner";
 import "./App.css";
 import { Skills } from "components/Skills";
-
+import { Projects } from "components/Projects";
 
 export const App = () =>  {
   return (
     <div  className="App">
       <NavBar></NavBar>
       <Banner />
-      
       <Skills />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
+
+
+
+
+export const Projects = () => {
+    const projects = [
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+      ];
+
+  return (
+    <div>
+        <Container>
+
+        </Container>
+
+    </div>
+  )
+}
