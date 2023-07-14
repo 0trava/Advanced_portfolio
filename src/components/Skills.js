0e2bb9a95846274b69d2e2f25fb3f29ee717b347
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-14 20:21:07 +0300

    add skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Container, Row, Col, Carousel } from 'react-bootstrap';
 import 'react-multi-carousel/lib/styles.css';
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp2.png";
 
 export const Skills = () => {
     const responsive = {
@@ -36,12 +40,36 @@ export const Skills = () => {
                 </h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                 <Carousel responsive={responsive} infinite={true} className='skill-slider'>
-                    
+                    <div className='item'>
+                        <img src={meter1} alt="Images"/>
+                        <h5>Web Development</h5>
+                    </div>
+                    <div className='item'>
+                        <img src={meter2} alt="Images"/>
+                        <h5>Web Development</h5>
+                    </div>
+                    <div className='item'>
+                        <img src={meter3} alt="Images"/>
+                        <h5>Web Development</h5>
+                    </div>
+                    <div className='item'>
+                        <img src={meter1} alt="Images"/>
+                        <h5>Web Development</h5>
+                    </div>
+                    <div className='item'>
+                        <img src={meter2} alt="Images"/>
+                        <h5>Web Development</h5>
+                    </div>
+                    <div className='item'>
+                        <img src={meter3} alt="Images"/>
+                        <h5>Web Development</h5>
+                    </div>
                 </Carousel>
             </div>
             </Col>
         </Row>
     </Container>
+    <img src={colorSharp} className='background-image-left' />
 
     </section>
   )
