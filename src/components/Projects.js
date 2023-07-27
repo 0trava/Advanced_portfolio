53ae891dcc43182e62c72701892d73864d233ab5
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-27 17:06:41 +0300

    add filter

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Container, Row, Col, Nav, Tab } from 'react-bootstrap';
 import projImg1 from "../assets/img/project-img-1.png";
 import projImg2 from "../assets/img/project-img-2.png";
@@ -9,58 +8,90 @@ import projImg6 from "../assets/img/project-img-6.png";
 import projImg7 from "../assets/img/project-img-7.png";
 import { ProjectCard } from './ProjectCard';
 import colorSharp from "../assets/img/color-sharp2.png";
+import { useState } from 'react';
 
 
 
 
 
 export const Projects = () => {
+
+
+
+
     const projects = [
         {
           title: "TagGame",
           description: "Design & Development",
           imgUrl: projImg1,
           link: "https://0trava.github.io/TagGame/",
+          type: "Pet-project",
         },
         {
           title: "Miami",
           description: "Practic project",
           imgUrl: projImg2,
           link: "https://0trava.github.io/cool-gallery-miami/",
+          type: "Practic work",
         },
         {
           title: "Natural Forest",
           description: "Practic project",
           imgUrl: projImg3,
           link: "https://0trava.github.io/Natural_Forest/",
+          type: "Practic work",
         },
         {
           title: "Mimino",
           description: "Group project",
           imgUrl: projImg4,
           link: "https://gitdenlev.github.io/codeincalidor/",
+          type: "Developer",
         },
         {
           title: "Barbershop",
           description: "Development",
           imgUrl: projImg5,
           link: "https://0trava.github.io/Barbershop/",
+          type: "Developer",
         },
         {
           title: "Filmoteka",
           description: "Group project",
           imgUrl: projImg6,
           link: "https://0trava.github.io/Filmoteka_group-project/",
+          type: "Developer",
         },
         {
           title: "WebStudio",
           description: "Development",
           imgUrl: projImg7,
           link: "https://0trava.github.io/goit-markup-hw-08/",
+          type: "Developer",
         },
 
 
       ];
+
+  const [projectList, setProjectList] = useState(projects);
+
+  const changeProjectList = (e) => {
+    const clickList = e.target.innerText;
+    console.log(e.target.innerText);
+
+    if (clickList) {
+      const result = projects.filter(project => project.type === clickList);
+      console.log(result);
+      setProjectList(result);
+    }
+  }
+
+  // useEffect (() => {
+
+  // }, changeProjectList)
+
+
+
 
   return (
     <section className='project' id="projects">
@@ -71,22 +102,22 @@ export const Projects = () => {
                 <p>Everything starts with an idea, but good realization comes with good developer</p>
 
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                <Nav onClick={changeProjectList} variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item >
+                      <Nav.Link eventKey="first">Developer</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      <Nav.Link eventKey="second">Practic work</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link  eventKey="third">Pet-project</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content>
                   <Tab.Pane eventKey="first">
                       <Row className='projects-row'>
                         {
-                          projects.map((project, index) => {
+                          projectList.map((project, index) => {
                             return (
                               <ProjectCard
                                 key={index}
@@ -97,6 +128,7 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
+
                     <Tab.Pane eventKey="section">
                       <p>Having a list of projects is a great way to show off your skills and abilities.</p>
                     </Tab.Pane>
